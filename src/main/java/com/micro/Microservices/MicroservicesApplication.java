2218a335a279f2f8769bbbbf0ec13282d34860b8
Vinit Solanki <vinit.solanki@everestek.com>
2024-12-29 18:59:42 +0530

    add main class

---
+++ src/main/java/com/micro/Microservices/MicroservicesApplication.java
@@ -0,0 +1,15 @@
+package com.micro.Microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+	//inital commit
+	// ye bhi inital commit
+}
