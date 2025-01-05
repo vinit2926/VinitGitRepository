6321f800cf4b1f870de44abf8371cc992bf98a2c
Vinit Solanki <vinit.solanki@everestek.com>
2025-01-05 14:30:28 +0530

    create user entity and add some fields

---
+++ src/main/java/com/micro/Microservices/entities/User.java
@@ -0,0 +1,10 @@
+package com.micro.Microservices.entities;
+
+public class User {
+
+
+    private String id;
+    private String username;
+    private String password;
+    private String gender;
+}
